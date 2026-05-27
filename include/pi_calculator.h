fc8ad529cbd3e9e6e23f2d4c7671ba20808c21b6
Huu Thien <dhthiendp1@gmail.com>
2026-05-27 09:41:49 +0700

    Update pi_calculator.h

--- include/pi_calculator.h
+++ include/pi_calculator.h
@@ -26,3 +26,21 @@ void PI_Calc_Current(Motor_Params* params, PI_Parameters* current_pi);
 void PI_Calc_Speed(Motor_Params* params, PI_Parameters* speed_pi);
 
 #endif // PI_CALCULATOR_H
+
+/*
+Motor_Params Motor_E_params = {
+        .L = MOTOR_LS,     
+        .R = MOTOR_LS,         
+        .Vdc = VDC_BUS,       
+        .PWM_FREQ = MOTOR_PWM_FREQ, 
+        .Tss = MOTOR_Tss,       
+        .m = MOTOR_m,       
+        .CFN3 = MOTOR_CFN3
+};
+
+PI_Parameters pi_current;
+PI_Parameters pi_speed;
+
+PI_Calc_Current(&Motor_E_params, &pi_current);
+PI_Calc_Speed(&Motor_E_params, &pi_speed);
+*/
